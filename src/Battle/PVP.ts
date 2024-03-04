996dbabd304577c82048749c3e946d6507b1b5d2
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2024-03-04 12:04:35 -0300

    feat: refatorado requisito 10

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,22 +1,16 @@
-import Character from '../Character';
 import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP<T extends Fighter> extends Battle {
-  private _player1: T;
-  private _player2: T;
-
-  constructor(player1: T, player2: T) {
+  constructor(private player1: T, private player2: T) {
     super(player1);
-    this._player1 = player1;
-    this._player2 = player2;
   }
 
   fight(): number {
-    while (this._player1.lifePoints > 0 && this._player2.lifePoints > 0) {
-      this._player1.attack(this._player2);
-      this._player2.attack(this._player1);
+    while (this.player1.lifePoints > 0 && this.player2.lifePoints > 0) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
     }
-    return this._player1.lifePoints === -1 ? -1 : 1;
+    return this.player1.lifePoints === -1 ? -1 : 1;
   }
 }
